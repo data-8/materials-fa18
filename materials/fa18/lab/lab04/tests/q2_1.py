0ad5c4008cdc3ecedaa40fde85f2c51ae55e74e8
Niraj Rao <niraj.a.rao@berkeley.edu>
2018-09-09 21:29:26 -0700

    fa18/lab/lab04.push

---
+++ materials/fa18/lab/lab04/tests/q2_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> to_percentage(.35) == 35.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
